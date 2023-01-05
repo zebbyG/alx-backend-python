405e5985dae4771f9c643734e6ec6ab7f1ad760e
zebbyG <zebbynjau@gmail.com>
2023-01-05 20:15:36 +0300

    add file&test 100..

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -4,10 +4,10 @@
     Arguments: lst: Sequence[Any]"""
 
 
-from typing import Sequence, Any, Union
+import typing
 
 
-def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+def safe_first_element(lst: typing.Sequence[typing.Any]) -> typing.Union[typing.Any, None]:
     """Return the first element of lst if there is any, otherwise None."""
     if lst:
         return lst[0]
