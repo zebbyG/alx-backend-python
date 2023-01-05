8e2cb8631a1afe0f15169d2378c53443f988938c
zebbyG <zebbynjau@gmail.com>
2023-01-05 11:31:23 +0300

    add file&test 7

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import typing
+
+
+def to_kv(k: str, v: typing.Union[int, float]) -> typing.Tuple[str, float]:
+    """
+    param k:annotated string
+    :param v:annotated int or float
+    :return:a Tuple(string k as first element
+    """
+    return k, v
