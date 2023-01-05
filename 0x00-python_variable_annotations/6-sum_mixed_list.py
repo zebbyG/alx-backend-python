e85edefef0f1444e4f83fb67a780f0749eacda79
zebbyG <zebbynjau@gmail.com>
2023-01-05 10:57:35 +0300

    add file&test 6

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import typing
+"""
+to help static type checkers and linters accurately predict errors.
+"""
+
+
+def sum_mixed_list(mxd_lst: typing.List[typing.Union[int, float]]) -> float:
+    """
+    :param mxd_lst: a list of integers
+    :return: their sum as a float
+    """
+    return sum(mxd_lst)
