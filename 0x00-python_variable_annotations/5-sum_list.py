b6c08ea47586161936208404ebed6e5cfba8258b
zebbyG <zebbynjau@gmail.com>
2023-01-05 10:10:53 +0300

    add file&test 5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import typing
+
+
+def sum_list(input_list: typing.List[float]) -> float:
+    """
+    :param input_list: type-annotated list of floats
+    :return: sum of the input_list-float
+    """
+    return sum(input_list)
