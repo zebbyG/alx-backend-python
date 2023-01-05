369414aac300d3df3c89161ca13417ab20c93b00
zebbyG <zebbynjau@gmail.com>
2023-01-05 20:04:50 +0300

    add file&test 100..

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
-"""Contains Augmented code with the correct duck-typed annotations."""
-from typing import Any, Union, Sequence
+''' Description: Augment the following code with the correct
+                 duck-typed annotations
+    Arguments: lst: Sequence[Any]
+'''
+
+from typing import Sequence, Any, Union
 
 
-def safe_first_element(lst: Sequence[Any]) -> Union[Any, list,  None]:
-    """Return the first element of a list or None if the list is empty."""
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    ''' Return the first element of lst if there is any, otherwise None. '''
     if lst:
         return lst[0]
     else:
