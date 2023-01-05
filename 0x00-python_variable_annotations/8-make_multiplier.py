5fb794c82178e3f82037122168f611cba70a86e8
zebbyG <zebbynjau@gmail.com>
2023-01-05 12:14:22 +0300

    add file&test 8

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import typing
+
+
+def make_multiplier(multiplier: float) -> typing.Callable[[float], float]:
+    """
+    :param multiplier: is a float
+    :return a function that multiplies multiplier by a float
+    """
+    def num_multiplier(num: float) -> float:
+        """
+        :param num: a float to be multiplied by multiplier
+        :return: float(multiplier * num)
+        """
+        return multiplier * num
+    return num_multiplier
