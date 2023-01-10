7ae914e22dddc21aa72b392f69ee759c4f91b1fb
zebbyG <zebbynjau@gmail.com>
2023-01-10 13:50:36 +0300

    fix task 1

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -5,10 +5,10 @@ Async Comprehensions
 
 
 import typing
-import asyncio
-import random
+
 async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> typing.List[float]:
+    """ Return list of values yielded by async_generator."""
     return [value async for value in async_generator()]
